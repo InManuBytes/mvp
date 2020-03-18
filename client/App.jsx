ae0fe7feb7f95235b053b98c988783e23b680d57
Manuela Gomez <mmanu.gomez@gmail.com>
2020-03-17 23:42:20 -0400

    Header, sub-header and search bar added

--- client/App.jsx
+++ client/App.jsx
@@ -8,7 +8,29 @@ class App extends Component {
   }
   render() {
     return (
-      <div>Tweet-ku created by react</div>
+      <div className="ui segment">
+        <div className="ui middle aligned two column centered grid">
+          <div className="row">
+            <div className="column">
+              <h1 className="ui icon header">
+                <i className="crow icon"></i>
+                <div className="content">
+                  Tweet-Ku
+                  <div className="sub header">Tweet-powered haiku generator</div>
+                </div>
+              </h1>
+            </div>
+          </div>
+          <div className="column">
+            <div className="ui search">
+              <div className="ui icon input">
+                <input class="prompt" type="text" placeholder="Twitter handle" />
+                <i class="feather icon" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
