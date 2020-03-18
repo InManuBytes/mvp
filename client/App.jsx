c5cd5242e939938a3382ea0bc08b951bf233e5ba
Manuela Gomez <mmanu.gomez@gmail.com>
2020-03-18 00:26:04 -0400

    Added call to action button

--- client/App.jsx
+++ client/App.jsx
@@ -21,12 +21,22 @@ class App extends Component {
               </h1>
             </div>
           </div>
-          <div className="column">
-            <div className="ui search">
-              <div className="ui left icon input" data-tooltip="Enter a user's Twitter handle to begin">
-                <i class="at icon" />
-                <input class="prompt" type="text" placeholder="Twitter Handle" />
+          <div className="row">
+            <div className="column">
+              <div className="ui search">
+                <div className="ui left icon input" data-tooltip="Enter a user's Twitter handle to begin">
+                  <i class="at icon" />
+                  <input class="prompt" type="text" placeholder="Twitter Handle" />
+                </div>
               </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="column">
+              <button className="large ui button">
+                <i className="feather icon"></i>
+                Compose a Tweet-Ku
+              </button>
             </div>
           </div>
         </div>
