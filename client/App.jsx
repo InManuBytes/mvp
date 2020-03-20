5f55d20a987c8601e30432e831ac0bea7ba9119d
Manuela Gomez <mmanu.gomez@gmail.com>
2020-03-19 20:11:13 -0400

    Added styling and responsive for mobile

--- client/App.jsx
+++ client/App.jsx
@@ -8,59 +8,75 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="ui container">
-        <div className="ui segment">
-          <div className="ui middle aligned centered grid">
-            <div className="row">
-              <div className="twelve wide column">
-                <h1 className="ui icon header">
-                  <i className="crow icon"></i>
-                  <div className="content">
-                    Tweet-Ku
-                    <div className="sub header">Tweet-powered haiku generator</div>
-                  </div>
-                </h1>
-              </div>
-            </div>
-            <div className="row">
-              <div className="twelve wide column">
-                <div className="ui search">
-                  <div className="ui left icon input" data-tooltip="Enter a user's Twitter handle to begin">
-                    <i className="at icon" />
-                    <input className="prompt" type="text" placeholder="Twitter Handle" />
-                  </div>
+      <div className="ui middle aligned one column centered grid" style={{ height: '100vh' }}>
+        {/* user interaction */}
+        <div className="column" style={{ maxWidth: 450 }}>
+          <div className="ui segment">
+            <div className="ui one column centered grid">
+              {/* Title header */}
+              <div className="column">
+                <div className="ui basic center aligned segment">
+                  <h1 className="ui icon header">
+                    <i className="crow icon"></i>
+                    <div className="content">
+                      Tweet-Ku
+                      <div className="sub header">Tweet-powered haiku generator</div>
+                    </div>
+                  </h1>
                 </div>
               </div>
-            </div>
-            <div className="row">
-              <div className="twelve wide column">
-                <button className="large ui button">
-                  <i className="feather icon"></i>
-                  Compose a Tweet-Ku
-                </button>
+              <div className="column">
+                {/* user input */}
+                <div className="row">
+                  <div className="ui basic center aligned segment">
+                    <div className="ui search">
+                      <div className="ui left icon input focus" data-tooltip="Enter a user's Twitter handle to begin">
+                        <i className="at icon" />
+                        <input className="prompt" type="text" placeholder="Twitter Handle" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* Generate haiku button */}
+                <div className="row">
+                  <div className="ui basic center aligned segment">
+                    <button className="ui large button">
+                      <i className="feather icon"></i>
+                      Compose haiku
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="ui fluid raised card">
-          <div className="content">
-            <div className="ui grey inverted basic segment">
-              <div className="center aligned description">
-                <div className="ui left floated">
-                  <i className="quote left icon" />
-                </div>
-                {/* TODO make a list to split haiku sentences */}
-                The apparition of these faces in the crowd:
-                Petals on a wet, black bough.
-                <div className="ui right floated">
-                  <i className="quote right icon" />
+        {/* Generated haku */}
+        <div className="column" style={{ maxWidth: 450 }}>
+          {/* haiku card */}
+          <div className="ui fluid raised card">
+            <div className="content">
+              {/* haiku */}
+              <div className="ui grey inverted basic segment">
+                <div className="center aligned description">
+                  <div className="ui left floated">
+                    <i className="quote left icon" />
+                  </div>
+                    <div class="ui list">
+                      <div class="item">The apparition of these faces in the crowd:</div>
+                      <div class="item">Petals on a wet,</div>
+                      <div class="item">black bough.</div>
+                    </div>
+                  <div className="ui right floated">
+                    <i className="quote right icon" />
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="extra content">
-              <div className="right aligned author">
-                <i className="ui crow icon" />
-                Ezra Pound
+              {/* author */}
+              <div className="extra content">
+                <div className="right aligned author">
+                  <i className="ui crow icon" />
+                  Ezra Pound
+                </div>
               </div>
             </div>
           </div>
