6e26b224ee6e4c77775aae8966022f675afa8847
Manuela Gomez <mmanu.gomez@gmail.com>
2020-03-19 20:57:42 -0400

    Connected to server to get haiku

--- client/server.js
+++ client/server.js
@@ -1,5 +1,12 @@
 const Server = {
-  address: 'http://localhost:3000/haikus',
+  address: 'http://localhost:3000/users',
+  getHaiku: (user) => {
+    const queryString = `?q=${user}`;
+    const searchURL = Server.address + queryString;
+    return fetch(searchURL)
+      .then(result =>  result.json())
+      .catch(err => console.log(err));
+  }
 }
 
 export default Server;
