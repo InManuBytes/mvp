449f43c265777196ed92e724b5ca97d1fd4a05df
Manuela Gomez <mmanu.gomez@gmail.com>
2020-03-21 10:31:23 -0400

    Generates a random haiku from watson's analysis

--- server/controller.js
+++ server/controller.js
@@ -1,6 +1,26 @@
+const _ = require('lodash');
+
 module.exports = {
   getHaiku(req, res, next) {
-    const user = req.params.user;
-    console.log('getting haiku');
+    console.log('generating haiku');
+    const { analysis } = req;
+    const { subjects, objects, actions } = analysis;
+    /**
+     * our naive haiku will be made up of
+     *            object
+     *          subject action
+     *            object
+     */
+    const firstLine = _.sample(objects);
+    const secondLine = `${_.sample(subjects)} ${_.sample(actions)}`;
+    const lastLine = _.sample(objects);
+    const haiku = [firstLine, secondLine, lastLine];
+    const randomHaiku = {
+      user: req.params.user,
+      haiku
+    };
+    console.log(randomHaiku);
+    res.status(200).json(randomHaiku);
+    next();
   }
 }
