076c283f548d03a471dd19758030797d3fa0585f
Manuela Gomez <mmanu.gomez@gmail.com>
2020-03-21 10:00:12 -0400

    Analyzes tweets and collects sentences and subjects

--- server/middleware/watson.js
+++ server/middleware/watson.js
@@ -1,23 +1,30 @@
 const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+const _ = require('lodash');
 
 const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({ version: '2019-07-12' });
 
 const analyzeTweets = (req, res, next) => {
-  console.log('analyzing tweets for ', req.tweets);
+  // the tweets come in at req.tweets
   const analyzeParams = {
     'features': {
       'semantic_roles': {}
     },
-    'text': 'This is a tweet'
+    'text': req.tweets
   };
-  next();
-  // naturalLanguageUnderstanding.analyze(analyzeParams)
-  // .then(analysisResults => {
-  //   console.log(JSON.stringify(analysisResults, null, 2));
-  // })
-  // .catch(err => {
-  //   console.log('error:', err);
-  // });
+  naturalLanguageUnderstanding.analyze(analyzeParams)
+  .then(analysisResults => {
+    const sentences = analysisResults.result.semantic_roles;
+    const allSubjects = sentences.map(sentence => {
+      return _.toLower(sentence.subject.text);
+    });
+    // const uniqueSubjects = allSubjects.
+    console.log(JSON.stringify(allSubjects));
+    next();
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(404).send(err);
+  });
 }
 
 module.exports = { analyzeTweets };
