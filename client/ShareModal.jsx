26c2bb59e70d5416d27530eea8b03e4175ca15b4
Manuela Gomez <mmanu.gomez@gmail.com>
2020-03-23 18:22:16 -0400

    Added closing functionality

--- client/ShareModal.jsx
+++ client/ShareModal.jsx
@@ -6,15 +6,39 @@ class ShareModal extends React.Component {
     this.state = {
 
     }
+    this.closeModal = this.closeModal.bind(this);
   }
+
+  closeModal() {
+    const { close } = this.props
+    close()
+  }
+
   render() {
     const { show } = this.props;
     if (!show) {
       return null;
     }
-    return <div>
-      Hello Modal
-    </div>
+    return (
+      <div class="ui active modal">
+        <i class="close icon" onClick={this.closeModal}></i>
+        <div class="header">
+          Modal Title
+        </div>
+        <div class="image content">
+          <div class="image">
+            An image can appear on left or an icon
+          </div>
+          <div class="description">
+            A description can appear on the right
+          </div>
+        </div>
+        <div class="actions">
+          <div class="ui button">Cancel</div>
+          <div class="ui button">OK</div>
+        </div>
+      </div>
+    );
   }
 }
 
