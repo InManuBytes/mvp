a552b6bad02f094b25f710677bb3e8fcb8aecde8
Manuela Gomez <mmanu.gomez@gmail.com>
2020-03-23 17:54:29 -0400

    Share renamed to HaikuCard

--- client/index.jsx
+++ client/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Server from './server';
-import makeHaikuCard from './Share';
+import makeHaikuCard from './HaikuCard';
 import 'fomantic-ui-less/semantic.less';
 
 ReactDOM.render(<App server={Server} makeHaikuCard={makeHaikuCard} />, document.getElementById('app'));
